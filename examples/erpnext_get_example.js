d411abca4a758aa08781c0eb39dc3d9256f05434
quadtree <e@quad-tree.com>
2020-12-27 00:04:59 -0600

    updated example

--- examples/erpnext_get_example.js
+++ examples/erpnext_get_example.js
@@ -1,31 +1,25 @@
-require('dotenv').config();
+var ERPNext = require("node-api-erpnext");
 var btoa = require("btoa");
-
-/**
- *  Exmple 1, how to get a Document info from a resource
- */
-
-/** Example 1 */
 
 var bot_url = process.env.API_ERPNEXT_BASEURL;
 var bot_login_auth = process.env.API_ERPNEXT_TOKEN;
 
 // Initialization
-var ERPNext = require("../src/api_erpnext");
 var login_ops = {
     baseUrl: bot_url,
     token: btoa(bot_login_auth)
 };
 
-// use
+
 var erp = new ERPNext(login_ops);
-var resource = "Task/TASK-2020-00001";
-var data_json = {
-    "description": `<div>automated new description for ${resource}</div>`
-};
+var resource = "Task/TASK-2020-00001";  // doctype/resource-name 
+// var data_json = {
+//     "description": `<div>automated new description for ${resource}</div>`
+// };   // data_json for insert/update
+
 try {
     //console.log(JSON.stringify(data_json));
-    erp.get(resource, data_json)
+    erp.getDoctype(resource)
     .then((json_result) => {
         //  console.log(JSON.stringify(json_result));
         console.log(JSON.stringify(json_result));
